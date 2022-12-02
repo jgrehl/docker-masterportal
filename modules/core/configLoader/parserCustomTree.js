a4140b64dd790c570d57512ca2c274c84bf15946
Inka Dudek <inka.dudek@gv.hamburg.de>
2022-12-02 17:48:05 +0100

    FolderComponent for tree and tests
    BG-3346

--- modules/core/configLoader/parserCustomTree.js
+++ modules/core/configLoader/parserCustomTree.js
@@ -180,8 +180,8 @@ const CustomTreeParser = Parser.extend(/** @lends CustomTreeParser.prototype */{
                 this.addItem({
                     type: "folder",
                     parentId: parentId,
-                    name: folder.Titel,
-                    id: folder.id,
+                    // name: folder.Titel,
+                    // id: folder.id,
                     isFolderSelectable: isFolderSelectable,
                     level: level,
                     icon: "bi-plus-circle-fill",
