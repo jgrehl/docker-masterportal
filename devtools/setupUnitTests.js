13d058e65105def88eadd96b583765ec6f90867d
Inka Dudek <inka.dudek@gv.hamburg.de>
2022-12-06 06:43:41 +0100

    moved setupUnitTests.js to devtools
    BG-3511

--- devtools/setupUnitTests.js
+++ devtools/setupUnitTests.js
@@ -1,1 +1,14 @@
+const {config, enableAutoUnmount} = require("@vue/test-utils");
+
 global.ResizeObserver = require("resize-observer-polyfill");
+// renderStubDefaultSlot: https://test-utils.vuejs.org/migration/#shallowmount-and-renderstubdefaultslot
+config.global.renderStubDefaultSlot = true;
+// global.SVGElement: @see https://github.com/vuejs/core/issues/3590
+global.SVGElement = window.SVGElement;
+
+/**
+ * EnableAutoUnmount allows to automatically destroy Vue wrappers.
+ * No wrapper.destroy() is needed in each test.
+ * Destroy logic is passed as callback to hook Function.
+ */
+enableAutoUnmount(afterEach);
