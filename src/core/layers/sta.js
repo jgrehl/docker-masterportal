3b90718277359b856947b17d9fa461f1aeeb3bbc
Inka Dudek <inka.dudek@gv.hamburg.de>
2022-12-21 07:39:02 +0100

    adaptions after rebase due to commit 281c121

--- src/core/layers/sta.js
+++ src/core/layers/sta.js
@@ -441,7 +441,7 @@ STALayer.prototype.initializeConnection = function (onsuccess, updateOnly = fals
             this.get("layer").setStyle(this.get("style"));
         }
 
-        features.forEach(feature => {
+        // features.forEach(feature => {
             bridge.changeFeatureGFI(feature);
         });
 
@@ -451,6 +451,9 @@ STALayer.prototype.initializeConnection = function (onsuccess, updateOnly = fals
         if (typeof this.options.afterLoading === "function") {
             this.options.afterLoading(features);
         }
+        // if (typeof this.get("historicalLocations") === "number") {
+        //     this.getHistoricalLocationsOfFeatures();
+        // }
     // }, error => {
     //     if (typeof this.options.onLoadingError === "function") {
     //         this.options.onLoadingError(error);
