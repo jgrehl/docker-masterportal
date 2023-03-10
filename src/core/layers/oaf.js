58e81955b814dabeaf3275b480f65392e47d3ef7
Inka Dudek <inka.dudek@gv.hamburg.de>
2023-03-10 11:22:00 +0100

    changes from PR 4019, geojson clustering and gfi at webgl layers

--- src/core/layers/oaf.js
+++ src/core/layers/oaf.js
@@ -34,7 +34,7 @@ export default function OAFLayer (attrs) {
     // call the super-layer
     Layer.call(this, Object.assign(defaults, attrs), this.layer, !attrs.isChildLayer);
     this.set("style", this.getStyleFunction(attrs));
-    this.prepareFeaturesFor3D(this.layer.getSource().getFeatures());
+    // this.prepareFeaturesFor3D(this.layer.getSource().getFeatures());
     if (attrs.clusterDistance) {
         this.set("isClustered", true);
     }
