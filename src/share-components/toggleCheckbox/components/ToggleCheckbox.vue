60eb8b2b183d7e6fe0d0ca88238ab044c049c750
fmlgv <friedrich.mueller@gv.hamburg.de>
2023-03-20 15:39:32 +0100

    BG-3912: revert testing

--- src/share-components/toggleCheckbox/components/ToggleCheckbox.vue
+++ src/share-components/toggleCheckbox/components/ToggleCheckbox.vue
@@ -114,7 +114,7 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-    @import "~/mixins";
+    @import "~/css/mixins.scss";
     @import "~variables";
 
     div.toggleCheckboxComponent {
